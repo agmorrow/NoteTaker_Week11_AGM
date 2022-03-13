16947c890f28a53fdbff2a53db1407a24b6c2fd6
agmorrow <morrow.austin5@gmail.com>
2022-03-12 21:58:48 -0700

    started settign up routes

---
+++ Develop/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT = 3001;
+
+
+
+app.get('/', (req, res) => {
+    res.send(
+      `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+    );
+  });
