f040bd27ec5b0aae6f84b42b676fe076f6d98d77
agmorrow <morrow.austin5@gmail.com>
2022-03-14 16:02:00 -0600

    local host works

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { notes, uuid } = require('./db/db.json');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -8,8 +10,35 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-    res.send(
-      res.json()
-    );
-  });
+
+app.use(function(req, res, next) {
+  req.notes = "Notes";
+  console.log('im happening');
+  next();
+});
+
+const checkBodyForText = (req, res, next) => {
+  if (req.body.text.length === 0) {
+    return res.status(404).json({ error: 'You must pass text to create a todo'});
+  } else {
+    next();
+  }
+};
+
+app.get('/api/notes', (req, res) => {
+  console.log(req.method, 'Notes are awesome!');
+  res.json(notes);
+});
+
+
+app.post('/api/notes', checkBodyForText, (req, res) => {
+  console.log(req.body);
+  const newNote = {
+      text: req.body.text,
+      id: uuid(),
+  };
+  notes.push(newNote);
+  res.json(newNote);
+});
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
