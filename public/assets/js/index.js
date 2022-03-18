a03a6f8fe2d9f3f2a172f6728ccb71fdc3a88039
agmorrow <morrow.austin5@gmail.com>
2022-03-18 12:04:42 -0600

    almost working

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,6 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-console.log('file loades')
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
--- server.js
+++ server.js
@@ -11,22 +11,22 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.use(express.static('public'));
 
- 
-
-app.get('/notes', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 const readFromFile = util.promisify(fs.readFile);
 
 const writeToFile = (destination, content) =>
-fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => 
-err ? console.log(err) : console.log(`\nData written to ${destination}`)
-);
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.log(err) : console.log(`\nData written to ${destination}`)
+  );
 
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
@@ -47,24 +47,29 @@ app.get('/api/notes', (req, res) => {
 
 const checkBodyForText = (req, res, next) => {
   if (req.body.text.length === 0) {
-      return res.status(401).json({ error: 'You must pass text to create a note'});
+    return res.status(401).json({
+      error: 'You must pass text to create a note'
+    });
   } else {
-      next();
+    next();
   }
 };
 
-app.post('/api/notes', checkBodyForText, (req,res) => {
+app.post('/api/notes', checkBodyForText, (req, res) => {
   console.info(`${req.method} request received to add a note`);
 
-  const { title, text } = req.body;
+  const {
+    title,
+    text
+  } = req.body;
 
   if (req.body) {
     const notes = {
       title,
-      text, 
+      text,
       note_id: uniqid(),
     };
-    
+
     readAndAppend(notes, './db/db.json');
     res.json(`Note added successfully`);
   } else {
@@ -72,8 +77,32 @@ app.post('/api/notes', checkBodyForText, (req,res) => {
   }
 });
 
-app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, '/public/index.html'))
+app.delete("/api/notes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
+    if (err) throw err;
+    const dbData = JSON.parse(data);
+    const newArray = [];
+
+    for (let i = 0; i < dbData.length; i++) {
+      if (i !== id) {
+        const newNotes = {
+          title: dbData[i].title,
+          text: dbData[i].text,
+          id: newArray.length
+        };
+        newArray.push(newNotes);
+      }
+    }
+    fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(newArray, null, 2), (err) => {
+      if (err) throw err;
+      res.json(req.body);
+    });
+  });
+});
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port: ${PORT} 🚀`));
